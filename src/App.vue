e4c34a9f0797e0dd02ee8cd422d0eaadac386fb1
anubra266 <anubra266@gmail.com>
2020-11-17 02:43:43 +0100

    import Todos parent component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Todos },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Todo One", completed: true },
+        { id: 2, title: "Todo Two", completed: false },
+        { id: 3, title: "Todo Three", completed: true },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
