69f17c956f2ed3706183d2766d054a7fe4099c08
anubra266 <anubra266@gmail.com>
2020-11-17 03:02:57 +0100

    display todos

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-
+        <h1>Todos</h1>
+        <div v-for="todo in todos" :key="todo.id">
+            <h3>{{ todo.title }}</h3>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name:'todos'
+        name:'todos',
+        props:["todos"]
     }
 </script>
 
