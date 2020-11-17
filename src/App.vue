c0debf58f26024ecd5aa3866a3975c372092c50b
anubra266 <anubra266@gmail.com>
2020-11-17 04:45:01 +0100

    Update navigation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
+<script>
+import Header from "./components/layout/Header";
+
+export default {
+  name: "app",
+  components: { Header },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
+  text-align: left;
   color: #2c3e50;
 }
 
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header>
     <h1>TodoList</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
   </header>
 </template>
 
@@ -20,5 +24,15 @@ header {
 header a {
   color: #fff;
   padding-right: 5px;
+  text-decoration: none;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 </style>
